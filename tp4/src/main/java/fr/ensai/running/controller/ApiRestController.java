aac5d95c7fe58e552062f39e69c2cd461cf65542
yatoute <yatoute.mintoama@eleve.ensai.fr>
2025-04-07 22:14:51 +0000

    Ajoint des endpoints GET all Atheletes, Get athelet by id, DELETE Athelete and CREATE Athelete

--- tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
+++ tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
@@ -3,7 +3,13 @@ package fr.ensai.running.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +27,44 @@ public class ApiRestController {
      * Get all athletes
      */
     @GetMapping("/athlete")
-    public List<Athlete> allAthletes() {
+    public ResponseEntity <List<Athlete>> allAthletes() {
 
-        return athleteService.findAll();
+        List<Athlete> listAthletes = athleteService.findAll(); 
+
+        if (listAthletes.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(listAthletes);
     }
+
+    @GetMapping("/athlete/{id}")
+    public ResponseEntity<Athlete> getAthleteById(@PathVariable(value = "id") long id) {
+        Athlete athlete = athleteService.findById(id);
+        if(athlete==null){
+            return ResponseEntity.noContent().build();
+        }
+        
+        return ResponseEntity.ok(athlete);
+    }
+
+    @DeleteMapping("/delete-athlete/{id}")
+    public ResponseEntity<Athlete> deleteAthleteById(@PathVariable(value = "id") long id) {
+        Athlete athlete = athleteService.findById(id);
+        if (athlete != null) {
+            athleteService.deleteById(id);
+        }
+       
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/athlete")
+    public ResponseEntity<Athlete> getAthleteById(@RequestBody Athlete athlete) {
+
+        Athlete createAthlete = athleteService.save(athlete);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(createAthlete);
+
+    }
+
 
 }
